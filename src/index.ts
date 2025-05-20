360346942cff242be76b3a554c3db29d931a60fb
FarzadVav <farzad.vav.work@gmail.com>
2025-05-20 12:41:09 +0330

    fix: hooks index

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { classMerge } from "./classMerge.lib"
 // Utilities
 export { faNumbersToEnglish, checkNumericInputs } from "./form.utility";
 export { numbersRegex, persianCharsRegex, slashesRegex } from "./regex.utility"
+
+// Hooks
+export { useBrowser } from "./hooks/useBrowser"
